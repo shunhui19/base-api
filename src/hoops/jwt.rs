d29694decf471d31998e05b51f4defd410525c1b
frank <shunhui29@163.com>
2025-02-27 15:29:05 +0800

    jwt support

--- src/hoops/jwt.rs
+++ src/hoops/jwt.rs
@@ -1,4 +1,5 @@
-use jsonwebtoken::EncodingKey;
+use anyhow::Result;
+use jsonwebtoken::{decode, Algorithm, DecodingKey, EncodingKey, Validation};
 use serde::{Deserialize, Serialize};
 use time::{Duration, OffsetDateTime};
 
@@ -23,5 +24,15 @@ pub fn get_token(uid: impl Into<String>) -> Result<(String, i64)> {
         &EncodingKey::from_secret(config::get().jwt.secret.as_bytes()),
     )?;
     Ok((token, exp.unix_timestamp()))
+}
+
+#[allow(dead_code)]
+pub fn decode_token(token: &str) -> bool {
+    decode::<JwtClaims>(
+        token,
+        &DecodingKey::from_secret(config::get().jwt.secret.as_bytes()),
+        &Validation::new(Algorithm::HS256),
+    )
+    .is_ok()
 }
 
